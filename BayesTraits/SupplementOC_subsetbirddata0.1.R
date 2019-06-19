1d6e5f2dd7f477cf971e567f0a710007a3df93e8
Cristina Robinson <NeuroBio@users.noreply.github.com>
2019-06-19 09:37:13 -0500

    Separate Bayes and BrowniePhy analyses for clarity

---
+++ BayesTraits/SupplementOC_subsetbirddata0.1.R
@@ -0,0 +1,241 @@
+########
+#Coded by Kate T. Snyder
+#Last Modified 5-2-2018 
+#Built using RStudio Version 1.1.456
+#R Version 3.3.1
+#
+#ape_4.1  phytools_0.5-38   maps_3.1.0  btw_V1.0
+#BayesTraitsV2 
+########
+########
+
+## Subset bird data and generate associated trees
+## 
+## Outputs list of 7 items:
+## bothtree - subsetted tree
+## ditree (equal to bothtree)
+## df - subsetted dataframe
+## matevec - species-named vector of mating data values
+## songcontdata - species-named vector of song data values
+## matecol - column name for mating data
+## songcol - column name for song data
+subsetbirddata <- function(MateParam=c("Polygyny","EPP","OC","none"),SongParam=c("Song","Syllsong","Syllrep","Interval","Duration","Rate","Continuity","OC","none"), withsuboscines = FALSE, newdata = FALSE, newtree = FALSE, islog = TRUE, minmax = NA) {
+require(phytools)
+require(ape)
+require(maps)
+  
+output <- list()
+
+if (newdata == FALSE) {
+  alldatadfos <- as.data.frame(read.csv("OCPaperDataAll.csv"))
+} else {alldatadfos <- as.data.frame(read.csv(newdata))}
+
+if (newtree == FALSE) {
+  matezillatree <- read.nexus("OCtreeHack1000.nex")
+}
+
+if (withsuboscines == FALSE) {
+  suboscines <- c("Synallaxis_erythrothorax", "Synallaxis_brachyura", "Synallaxis_zimmeri", "Cranioleuca_erythrops", "Pseudoseisura_cristata", "Pseudoseisura_lophotes", "Phacellodomus_striaticollis", "Phacellodomus_maculipectus", "Leptasthenura_striolata", "Leptasthenura_yanacensis", "Cinclodes_fuscus", "Cinclodes_antarcticus", "Cinclodes_comechingonus", "Cinclodes_albiventris", "Furnarius_rufus", "Automolus_ochrolaemus", "Thripadectes_rufobrunneus", "Automolus_rubiginosus", "Xenops_minutus", "Sclerurus_guatemalensis", "Sclerurus_caudacutus", "Conopophaga_lineata", "Cercomacra_tyrannina", "Gymnopithys_leucaspis", "Empidonax_traillii", "Empidonax_alnorum", "Empidonax_minimus", "Empidonax_difficilis", "Empidonax_flaviventris", "Empidonax_virescens", "Contopus_sordidulus", "Sayornis_nigricans", "Sayornis_phoebe", "Pyrocephalus_rubinus", "Myiarchus_crinitus", "Myiarchus_cinerascens", "Tyrannus_tyrannus", "Elaenia_chiriquensis", "Oxyruncus_cristatus")
+  # suboscinetips <- match(suboscines,matezillatree$tip.label)
+  # oscinetree <- drop.tip(matezillatree, tip=suboscinetips)
+  # matezillatree <- oscinetree
+  # subosrows <- alldatadfos$BirdtreeFormat %in% suboscines
+  # alldatadf <- alldatadfos[which(!subosrows),]
+  alldatadf <- alldatadfos
+ } else if (withsuboscines == TRUE) {
+   alldatadf <- alldatadfos
+ }
+alldatadf <- alldatadf[which(alldatadf$BirdtreeFormat %in% matezillatree$tip.label),]  #two species are in dataset that aren't in overall tree, need to get them out of the dataset
+
+#Go through each mating system 
+if (MateParam == "Polygyny") {
+  matedf <- alldatadf[!is.na(alldatadf$Polygyny.final),]
+  havedatavec <- matezillatree$tip.label %in% as.character(matedf$BirdtreeFormat)
+  matingtips <- which(havedatavec == TRUE)
+  dropformating <- which(havedatavec == FALSE)
+  matingtree <- drop.tip(matezillatree, tip = dropformating)
+  matecol <- "Polygyny.final"
+} else if (MateParam == "EPP") {
+  matedf <- alldatadf[!is.na(alldatadf$EPP.final),] 
+  matecol <- "EPP.final"
+  havedatavec <- matezillatree$tip.label %in% as.character(matedf$BirdtreeFormat)
+  matingtips <- which(havedatavec == TRUE)
+  dropformating <- which(havedatavec == FALSE)
+  matingtree <- drop.tip(matezillatree, tip = dropformating)
+} else if (MateParam=="OC") {
+  matedf <- alldatadf[!is.na(alldatadf$O.C),] 
+  matecol <- "O.C"
+  havedatavec <- matezillatree$tip.label %in% as.character(matedf$BirdtreeFormat)
+  matingtips <- which(havedatavec == TRUE)
+  dropformating <- which(havedatavec == FALSE)
+  matingtree <- drop.tip(matezillatree, tip = dropformating)
+} else if (MateParam=="none") {
+  matedf <- alldatadf
+  matingtree <- matezillatree
+  matecol <- "None"
+} else {
+  return(print("Not a valid value for MateParam"))}
+
+
+#Go through all song metrics to subset data and tree
+if (SongParam == "Syllrep") {
+  songdf <- matedf[!is.na(matedf$Syllable.rep.final),]
+  havedatavec <- matingtree$tip.label %in% as.character(songdf$BirdtreeFormat)
+  matingtips <- which(havedatavec == TRUE)
+  dropformating <- which(havedatavec == FALSE)
+  bothtree <- drop.tip(matingtree, tip = dropformating)
+  songcol <- "Syllable.rep.final"
+  
+} else if (SongParam == "Syllsong") {
+  songdf <- matedf[!is.na(matedf$Syll.song.final),]
+  havedatavec <- matingtree$tip.label %in% as.character(songdf$BirdtreeFormat)
+  matingtips <- which(havedatavec == TRUE)
+  dropformating <- which(havedatavec == FALSE)
+  bothtree <- drop.tip(matingtree, tip = dropformating)
+  songcol <- "Syll.song.final"
+  
+} else if (SongParam == "Song") {
+  songdf <- matedf[!is.na(matedf$Song.rep.final),]
+  havedatavec <- matingtree$tip.label %in% as.character(songdf$BirdtreeFormat)
+  matingtips <- which(havedatavec == TRUE)
+  dropformating <- which(havedatavec == FALSE)
+  bothtree <- drop.tip(matingtree, tip = dropformating)
+  songcol <- "Song.rep.final"
+  
+} else if (SongParam == "Interval") {
+  songdf <- matedf[!is.na(matedf$Interval.final),]
+  havedatavec <- matingtree$tip.label %in% as.character(songdf$BirdtreeFormat)
+  matingtips <- which(havedatavec == TRUE)
+  dropformating <- which(havedatavec == FALSE)
+  bothtree <- drop.tip(matingtree, tip = dropformating)
+  songcol <- "Interval.final"
+  
+} else if (SongParam == "Duration") {
+  songdf <- matedf[!is.na(matedf$Duration.final),]
+  havedatavec <- matingtree$tip.label %in% as.character(songdf$BirdtreeFormat)
+  matingtips <- which(havedatavec == TRUE)
+  dropformating <- which(havedatavec == FALSE)
+  bothtree <- drop.tip(matingtree, tip = dropformating)
+  songcol <- "Duration.final"
+  
+} else if (SongParam == "Continuity") {
+  songdf <- matedf[!is.na(matedf$Continuity),]
+  havedatavec <- matingtree$tip.label %in% as.character(songdf$BirdtreeFormat)
+  matingtips <- which(havedatavec == TRUE)
+  dropformating <- which(havedatavec == FALSE)
+  bothtree <- drop.tip(matingtree, tip = dropformating)
+  songcol <- "Continuity"
+  
+} else if (SongParam == "Rate") {
+  songdf <- matedf[!is.na(matedf$Song.rate),]
+  havedatavec <- matingtree$tip.label %in% as.character(songdf$BirdtreeFormat)
+  matingtips <- which(havedatavec == TRUE)
+  dropformating <- which(havedatavec == FALSE)
+  bothtree <- drop.tip(matingtree, tip = dropformating)
+  songcol <- "Song.rate"
+} else if (SongParam == "OC") {
+  songdf <- matedf[!is.na(matedf$O.C),]
+  havedatavec <- matingtree$tip.label %in% as.character(songdf$BirdtreeFormat)
+  matingtips <- which(havedatavec == TRUE)
+  dropformating <- which(havedatavec == FALSE)
+  bothtree <- drop.tip(matingtree, tip = dropformating)
+  songcol <- "O.C"
+} else if (SongParam == "none") {
+  songdf <- matedf
+  songcol <- NULL
+  bothtree <- matingtree
+} else {return(print("Not a valid value for SongParam"))}
+
+
+if (MateParam != "none") {
+  matevec <- as.character(songdf[,matecol])
+  names(matevec) <- songdf$BirdtreeFormat
+} else {
+  matevec <- "none"
+}
+
+#Extract data vectors for song traits
+if (is.na(minmax)) {
+  if (SongParam == "Syllrep") {
+    songcol <- "Syllable.rep.final"
+  } else if (SongParam == "Syllsong") {
+    songcol <- "Syll.song.final"
+  } else if (SongParam == "Song") {
+    songcol <- "Song.rep.final"
+  } else if (SongParam == "Interval") {
+    songcol <- "Interval.final"
+  } else if (SongParam == "Duration") {
+    songcol <- "Duration.final"
+  } else if (SongParam == "Rate") {
+    songcol <- "Song.rate"
+  } else if (SongParam == "Continuity") {
+    songcol <- "Continuity"
+  } else if (SongParam == "OC") {
+    songcol <- "O.C"
+  } else if (SongParam == "none") {
+    songdatavec <- matevec
+  } 
+  
+} else if (minmax == "min") {
+  if (SongParam == "Syllrep") {
+    songcol <- "Syllable.rep.min"
+  } else if (SongParam == "Syllsong") {
+    songcol <- "Syll.song.min"
+  } else if (SongParam == "Song") {
+    songcol <- "Song.rep.min"
+  } else if (SongParam == "Interval") {
+    songcol <- "Interval.min"
+  } else if (SongParam == "Duration") {
+    songcol <- "Duration.min"
+  } else if (SongParam == "Rate") {
+    songcol <- "Song.rate.min"
+  } else if (SongParam == "Continuity") {
+    songcol <- "Continuity.min"
+  } else if (SongParam == "none") {
+    songdatavec <- matevec
+  } 
+} else if (minmax == "max") {
+  if (SongParam == "Syllrep") {
+    songcol <- "Syllable.rep.max"
+  } else if (SongParam == "Syllsong") {
+    songcol <- "Syll.song.max"
+  } else if (SongParam == "Song") {
+    songcol <- "Song.rep.max"
+  } else if (SongParam == "Interval") {
+    songcol <- "Interval.max"
+  } else if (SongParam == "Duration") {
+    songcol <- "Duration.max"
+  } else if (SongParam == "Continuity") {
+    songcol <- "Continuity.max"
+  } else if (SongParam == "Rate") {
+    songcol <- "Song.rate.max"  
+  } else if (SongParam == "none") {
+    songdatavec <- matevec
+  } 
+}
+
+
+ditree <- multi2di(bothtree)
+ditree$edge.length[ditree$edge.length == 0] <- 0.0000000000000000001
+
+output$ditree <- ditree
+output$bothtree <- bothtree
+output$matecol <- matecol
+output$matevec <- matevec
+output$df <- songdf
+
+if (SongParam != "none") {
+songcontvec <- as.numeric(songdf[,songcol])
+
+if (islog == TRUE) {
+  songcontvec <- log(songcontvec,10)
+}
+names(songcontvec) <- songdf$BirdtreeFormat
+output$songcol <- songcol
+output$songcontvec <- songcontvec
+} #end if SongParam != "none"
+
+return(output)
+}
+
+
